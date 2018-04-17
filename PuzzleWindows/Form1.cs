9ff8fc2fd29bddf16efd043d6dca8815d43c4c98
Taeeun Kim <iostream1701@gmail.com>
2018-04-17 10:41:04 +0900

    퍼즐게임 만드는중

---
+++ PuzzleWindows/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuzzleWindows
+{
+    public partial class Form1 : Form
+    {
+        Pen thePen;
+        Brush theBrush;
+        Font theFont;
+        List<Image> imgList;
+        private int puzzleSize = 16;
+        int imgWidth = 100;
+        int imgHeight = 100;
+        PuzzleGameEngine pge;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            imgList = new List<Image>();
+
+            for(int i = 0; i < puzzleSize; i++)
+            {
+                string fileName = string.Format("pic_{0}.png",(char)('a'+i));
+                Image tmpI = Image.FromFile(fileName);
+                imgList.Add(tmpI);
+            }
+            
+            thePen = new Pen(Color.Red);
+            theBrush = new SolidBrush(Color.Green);
+            theFont = new Font("굴림", 15);
+
+            pge = new PuzzleGameEngine();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            String strTime = String.Format("Time:{0:D3}", 99);
+
+            e.Graphics.DrawRectangle(thePen, 0, 0, 100, 10);
+            e.Graphics.DrawString(strTime, theFont, theBrush, 0, 10);
+            for(int i = 0; i < 16; i++)
+            {
+                if(pge.GetViewIndex(i) != puzzleSize-1)
+                    e.Graphics.DrawImage(imgList[pge.GetViewIndex(i)], imgWidth * (i%4)+50, imgHeight * (i/4)+50, imgWidth, imgHeight);
+
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int tmpX = e.X;
+            int tmpY = e.Y;
+            MessageBox.Show(tmpX + "," + tmpY);
+        }
+    }
+}
---
+++ PuzzleWindows/PuzzleGameEngine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PuzzleWindows
+{
+    internal class PuzzleGameEngine
+    {
+        int[] theViewIndices;
+        private int puzzleSize = 16;
+
+        public PuzzleGameEngine()
+        {
+            //이미지 index 가져오자
+            theViewIndices = new int[puzzleSize];
+            for (int a = 0; a < puzzleSize; a++)
+            {
+                theViewIndices[a] = a;
+            }
+
+            //이미지 index 섞자
+            int i, j;
+            Random r = new Random();
+            for (int n = 0; n < 100; n++)
+            {
+                i = r.Next(0, puzzleSize);
+                j = r.Next(0, puzzleSize);
+                Swap(i,j);
+            }
+        }
+
+        private void Swap(int i1, int i2)
+        {
+            int temp;
+            temp = theViewIndices[i1];
+            theViewIndices[i1] = theViewIndices[i2];
+            theViewIndices[i2] = temp;
+        }
+
+        internal int GetViewIndex(int index)
+        {
+            return theViewIndices[index];
+        }
+    }
+}
